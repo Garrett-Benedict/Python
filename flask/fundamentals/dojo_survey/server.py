0115bb7e13ac46edfbb2ae213f079561c99adcc9
Garrett Benedict <garrett.benedict.080@gmail.com>
2021-07-14 14:51:18 -0700

    Week 2

--- flask/fundamentals/dojo_survey/server.py
+++ flask/fundamentals/dojo_survey/server.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 
 survey = Flask(__name__)
+survey.secret_key = "account info"
 
 @survey.route("/")
 def index():
@@ -8,6 +9,10 @@ def index():
 
 @survey.route("/result", methods = ["POST"])
 def result():
+    session['name'] = request.form['name']
+    session['loca'] = request.form['loca']
+    session['lang'] = request.form['lang']
+    session['comm'] = request.form['comm']
     return render_template (
     "display.html",
     name = request.form['name'],
